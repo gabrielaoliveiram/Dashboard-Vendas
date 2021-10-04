834844ee306d4d9249da20e1d83e3b05ac7350d8
gabrielavmzs@gmail.com <gabrielamenezes@mec.gov.br>
2021-10-04 09:50:33 -0300

    first commit

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,49 @@
+const DataTable = () => {
+    return (
+        <div className="table-responsive">
+            <table className="table table-striped table-sm">
+                <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes Visitados</th>
+                        <th>Negócios Fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Barry Allen</th>
+                        <th>34</th>
+                        <th>25</th>
+                        <th>15017.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Barry Allen</th>
+                        <th>34</th>
+                        <th>25</th>
+                        <th>15017.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Barry Allen</th>
+                        <th>34</th>
+                        <th>25</th>
+                        <th>15017.00</th>
+                    </tr>
+                    <tr>
+                        <th>22/04/2021</th>
+                        <th>Barry Allen</th>
+                        <th>34</th>
+                        <th>25</th>
+                        <th>15017.00</th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default DataTable;
